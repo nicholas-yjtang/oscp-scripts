d3100399d0446e90f09a62a50c55de85ab34b4ab
Nicholas Tang <nicholas.yjtang@gmail.com>
2026-06-20 03:57:38 +0800

    made modification on the mkdir script based on the changes on the repository

--- mkdir.sh
+++ mkdir.sh
@@ -6,15 +6,15 @@ if [ -z "$NEWDIR" ]; then
   exit 1
 fi
 
-pushd $SCRIPTDIR
 if [ -d "$NEWDIR" ]; then
     echo "Directory $1 already exists."
     exit 1
 fi
 
+SCRIPTDIR_NAME=$(basename $SCRIPTDIR)
 mkdir -p $NEWDIR
 if [ ! -f $NEWDIR/setup.sh ]; then
-    default_setup=$(find . -name "setup.sh" | grep "default")
+    default_setup=$(find $SCRIPTDIR -name "setup.sh" | grep "default")
     cp "$default_setup" $NEWDIR
     echo "NEWDIR: $NEWDIR"
     readarray -td "/" directories <<< "$NEWDIR"    
@@ -24,32 +24,31 @@ if [ ! -f $NEWDIR/setup.sh ]; then
     for i in $(seq 1 $number_of_directories); do
         subdir+='\.\.\/'
     done
-    sed -i "s/scripts\/common/$subdir""scripts\/common/g" $NEWDIR/setup.sh
+    sed -i "s/scripts\/common/$subdir""$SCRIPTDIR_NAME\/common/g" $NEWDIR/setup.sh
 fi
 
 if [ ! -f $NEWDIR/walk.sh ]; then
-    default_walk=$(find . -name "walk.sh" | grep "default")
+    default_walk=$(find $SCRIPTDIR -name "walk.sh" | grep "default")
     cp "$default_walk"  $NEWDIR
 fi
 
 if [ ! -f $NEWDIR/mkproject.sh ]; then
-    default_project_setup=$(find . -name "mkproject.sh" | grep "default")
+    default_project_setup=$(find $SCRIPTDIR -name "mkproject.sh" | grep "default")
     cp "$default_project_setup" $NEWDIR
 fi
 
 if [ ! -f $NEWDIR/setup_network.sh ]; then
-	default_setup_network=$(find . -name "setup_network.sh" | grep "default")
+	default_setup_network=$(find $SCRIPTDIR -name "setup_network.sh" | grep "default")
 	cp $default_setup_network $NEWDIR
 fi
 
 if [ ! -f $NEWDIR/mkpart.sh ]; then
-	default_setup_network=$(find . -name "mkpart.sh" | grep "default")
+	default_setup_network=$(find $SCRIPTDIR -name "mkpart.sh" | grep "default")
 	cp $default_setup_network $NEWDIR
 fi
 
 if [ ! -f $NEWDIR/part_template.sh ]; then
-	default_setup_network=$(find . -name "part_template.sh" | grep "default")
+	default_setup_network=$(find $SCRIPTDIR -name "part_template.sh" | grep "default")
 	cp $default_setup_network $NEWDIR
 fi
 
-popd
